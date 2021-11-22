88007c70a32d5e0a61f3eac3b142384ec01030d4
Xff7Xsephiroth <55700964+Xff7Xsephiroth@users.noreply.github.com>
2021-11-22 14:59:32 +0000

    Voice control implemented
    Voice command works spawning a cube, will try sphere next to make sure the script is listening for 2 specific words, then will implement 2 separate scripts. one for spell use and another for giving ally AI commands. with stretch to summoning ability.

---
+++ VR project 3000/Assets/VoiceRecog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+
+public class VoiceRecog : MonoBehaviour
+{
+    public GameObject CubeTest;
+    private KeywordRecognizer SpellKeywordRecog;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    private void Start()
+    {
+        actions.Add("cube", Cube);
+       // actions.Add("sphere", Sphere);
+
+
+        SpellKeywordRecog = new KeywordRecognizer(actions.Keys.ToArray());
+        SpellKeywordRecog.OnPhraseRecognized += SpeechDetected;
+        SpellKeywordRecog.Start();
+    }
+
+    private void SpeechDetected(PhraseRecognizedEventArgs speech)
+
+    {
+        Debug.Log(speech.text);
+
+        actions[speech.text].Invoke();
+
+    }
+
+    private void Cube()
+    {
+        Debug.Log("test");
+        Instantiate(CubeTest);
+    }
+
+    private void SphereSpawn()
+    {
+      //  Instantiate(Sphere(), new Vector3(newX, newZ, 1), Quaternion.identity);
+    }
+}
