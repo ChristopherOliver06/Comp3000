35442544860b402d2c8a8defd2a09510e2aec7ec
Xff7Xsephiroth <55700964+Xff7Xsephiroth@users.noreply.github.com>
2022-02-22 14:33:52 +0000

    bit more added visually
    skybox imported and a couple of prefab enemies (won't get used) created scripts for ally command and created 12 different commands to give allies. created health bars and names for allies and enemies. started to make main menu and work on the first town (tutorial place)

---
+++ VR project 3000/Assets/Scripts/AllyCommands.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class AllyCommands : MonoBehaviour
+
+{
+    public GameObject AttackCommand;
+    public GameObject DefenceCommand;
+    public GameObject FireBallCommand;
+    public GameObject IceBlastCommand;
+    public GameObject ThunderSlapCommand;
+    public GameObject BarrierCommand;
+    public GameObject MagicBarrierCommand;
+    public GameObject HealCommand;
+    public GameObject AntigenCommand;
+
+    private KeywordRecognizer allyCommandKeyword;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+
+    private void Start()
+    {
+
+
+        allyCommandKeyword = new KeywordRecognizer(actions.Keys.ToArray());
+        allyCommandKeyword.OnPhraseRecognized += SpeechDetected;
+        allyCommandKeyword.Start();
+    }
+
+
+    private void SpeechDetected(PhraseRecognizedEventArgs speech)
+
+    {
+        Debug.Log(speech.text);
+
+        actions[speech.text].Invoke();
+
+    }
+
+
+    private void Attack()
+    {
+        Debug.Log("test");
+
+        Instantiate(AttackCommand);
+
+    }
+
+    private void Defence()
+    {
+        Debug.Log("test");
+
+        Instantiate(DefenceCommand);
+
+    }
+
+
+    private void FireBall()
+    {
+        Debug.Log("test");
+
+        Instantiate(FireBallCommand);
+
+    }
+
+    private void IceBlast()
+    {
+        Debug.Log("test");
+
+        Instantiate(IceBlastCommand);
+
+    }
+
+    private void ThunderSlap()
+    {
+        Debug.Log("test");
+
+        Instantiate(ThunderSlapCommand);
+
+    }
+    private void Barrier()
+    {
+        Debug.Log("test");
+
+        Instantiate(BarrierCommand);
+
+    }
+
+    private void MagicBarrier()
+    {
+        Debug.Log("test");
+
+        Instantiate(MagicBarrierCommand);
+
+    }
+    private void Heal()
+    {
+        Debug.Log("test");
+
+        Instantiate(HealCommand);
+
+    }
+
+    private void Antigen()
+    {
+        Debug.Log("test");
+
+        Instantiate(AntigenCommand);
+
+    }
+
+}
